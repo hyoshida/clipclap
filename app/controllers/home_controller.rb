8d6ce961425870bb0db7d52e7a477133dd802f10
YOSHIDA Hiroki <hyoshida@appirits.com>
2012-10-15 01:13:05 +0900

    ホーム画面: デザイン作り込み

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,27 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.paginate(page: params[:page])
+    if first_page?
+      render
+    else
+      render text: "<html><body><div id='container'>#{@posts.map {|post| create_image_tag(post) }.join}</div></body></html>"
+    end
+  end
+
+  private
+
+  def create_image_tag(post)
+    <<-EOS
+    <ul>
+      <li class='box'>
+        <img src='#{post.url}' />
+        #{post.title.present? ? '<p class="title">' + post.title + '</p>' : ''}
+      </li>
+    </ul>
+    EOS
+  end
+
+  def first_page?
+    params[:page].blank? || params[:page].to_i <= 1
   end
 end
