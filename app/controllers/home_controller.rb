901d64908ee0cf21ca628de279d0942862718c8a
YOSHIDA Hiroki <hyoshida@appirits.com>
2012-10-22 02:25:46 +0900

    ホーム画面: リファクタリング

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,22 +4,11 @@ class HomeController < ApplicationController
     if first_page?
       render
     else
-      render text: "<html><body><div id='container'>#{@posts.map {|post| create_image_tag(post) }.join}</div></body></html>"
+      render action: 'next_page'
     end
   end
 
   private
-
-  def create_image_tag(post)
-    <<-EOS
-    <ul>
-      <li class='box'>
-        <img src='#{post.url}' />
-        #{post.title.present? ? '<p class="title">' + post.title + '</p>' : ''}
-      </li>
-    </ul>
-    EOS
-  end
 
   def first_page?
     params[:page].blank? || params[:page].to_i <= 1
