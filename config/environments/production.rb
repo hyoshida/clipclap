53b56ba117e63de0d5592556dae982437c126f5f
YOSHIDA Hiroki <hyoshida@appirits.com>
2012-11-11 06:04:10 +0900

    アプリケーションをサブディレクトリで正常に動作させるための設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,7 @@ Postclip::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # added by yoshida 2012-11-11
+  config.action_controller.relative_url_root = "/postclip"
 end
