9e346dc6c43fbd609d2b826da9d56aad5a2a7e32
YOSHIDA Hiroki <hyoshida@appirits.com>
2012-11-11 06:33:05 +0900

    ウォール: デザインを微調整

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@ Postclip::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( gallery/*.js gallery/*.css )
+  config.assets.precompile += %w( wall.js posts_form.js gallery/*.js gallery/*.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
