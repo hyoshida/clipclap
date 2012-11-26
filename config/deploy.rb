e9fd48030972a816126c879527abe0ece77aa92f
YOSHIDA Hiroki <hyoshida@appirits.com>
2012-11-27 00:37:09 +0900

    ドメイン失効につきサーバの設定を修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'bundler/capistrano'
 
 set :application, 'postclip'
-set :repository,  'git@codepia.co.cc:postclip.git'
+set :repository,  'git@49.212.160.224:postclip.git'
 set :deploy_to, "/home/yoshida/public_html/#{application}"
 set :rails_env, 'production'
 set :user, 'yoshida'
@@ -10,9 +10,9 @@ set :use_sudo, false
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, 'codepia.co.cc'                          # Your HTTP server, Apache/etc
-role :app, 'codepia.co.cc'                          # This may be the same as your `Web` server
-role :db,  'codepia.co.cc', :primary => true # This is where Rails migrations will run
+role :web, '49.212.160.224'                          # Your HTTP server, Apache/etc
+role :app, '49.212.160.224'                          # This may be the same as your `Web` server
+role :db,  '49.212.160.224', :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
