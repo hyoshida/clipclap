f9addd0904fe169fb765bd31196bf8a6ff9fad32
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-10-26 19:03:22 +0900

    refs #2 フォロワー一覧を実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@
 //= require bootstrap-tooltip
 //= require bootstrap-popover
 //= require bootstrap-dropdown
+//= require bootstrap-carousel
 //= require masonry/jquery.masonry
 //= require masonry/jquery.imagesloaded.min
 //= require masonry/jquery.infinitescroll.min
