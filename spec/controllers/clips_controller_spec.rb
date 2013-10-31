69306351a1787b91fd775b8518f627ce56986503
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-10-31 22:52:27 +0900

    refs #7 リクリップ: テストに未ログイン時のケースを追加

--- spec/controllers/clips_controller_spec.rb
+++ spec/controllers/clips_controller_spec.rb
@@ -3,41 +3,84 @@ require 'spec_helper'
 describe ClipsController do
   let (:clip) { FactoryGirl.create(:clip) }
 
-  before_sign_in
   before { clip }
 
   describe "PUT 'reclip'" do
-    it "returns http error" do
-      expect { put :reclip, id: clip.id }.to raise_error(ActionView::MissingTemplate)
+    context "未ログインの場合" do
+      it "ログインページにリダイレクトすること" do
+        put :reclip, id: clip.id
+        response.should redirect_to(:new_user_session)
+      end
+    end
+
+    context "ログイン済みの場合" do
+      before_sign_in
+      it "returns http error" do
+        # TODO: 正常にリクリップできるようにする
+        expect { put :reclip, id: clip.id }.to raise_error(ActionView::MissingTemplate)
+      end
     end
   end
 
   describe "PUT 'unreclip'" do
-    it "returns http error" do
-      expect { put :unreclip, id: clip.id }.to raise_error(ActionView::MissingTemplate)
+    context "未ログインの場合" do
+      it "ログインページにリダイレクトすること" do
+        put :reclip, id: clip.id
+        response.should redirect_to(:new_user_session)
+      end
+    end
+
+    context "ログイン済みの場合" do
+      before_sign_in
+      it "returns http error" do
+        # TODO: 正常にリクリップできるようにする
+        expect { put :unreclip, id: clip.id }.to raise_error(ActionView::MissingTemplate)
+      end
     end
   end
 
   describe "XHR-PUT 'reclip'" do
-    before { xhr :put, :reclip, id: clip.id }
+    context "未ログインの場合" do
+      it "ログインページにリダイレクトすること" do
+        xhr :put, :reclip, id: clip.id
+        response.body.should include('window.location')
+        response.body.should include(new_user_session_path)
+      end
+    end
 
-    its (:response) { should be_success }
-    its (:current_user) { should be_present }
+    context "ログイン済みの場合" do
+      before_sign_in
+      before { xhr :put, :reclip, id: clip.id }
 
-    it 'リクリップに成功していること' do
-      assigns[:clip].parent_id.should eq(clip.id)
+      its (:response) { should be_success }
+      its (:current_user) { should be_present }
+
+      it 'リクリップに成功していること' do
+        assigns[:clip].parent_id.should eq(clip.id)
+      end
     end
   end
 
   describe "XHR-PUT 'unreclip'" do
-    before { xhr :put, :reclip, id: clip.id }
-    before { xhr :put, :unreclip, id: clip.id }
+    context "未ログインの場合" do
+      it "ログインページにリダイレクトすること" do
+        xhr :put, :unreclip, id: clip.id
+        response.body.should include('window.location')
+        response.body.should include(new_user_session_path)
+      end
+    end
 
-    its (:response) { should be_success }
-    its (:current_user) { should be_present }
+    context "ログイン済みの場合" do
+      before_sign_in
+      before { xhr :put, :reclip, id: clip.id }
+      before { xhr :put, :unreclip, id: clip.id }
 
-    it 'リクリップ解除に成功していること' do
-      assigns[:clip].destroyed?.should be_true
+      its (:response) { should be_success }
+      its (:current_user) { should be_present }
+
+      it 'リクリップ解除に成功していること' do
+        assigns[:clip].destroyed?.should be_true
+      end
     end
   end
 end
