47698dec99c0095c19dbe9cb61bb694ca2fe9cd1
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-11-14 23:04:41 +0900

    新規投稿: アスペクト比のバリデーションが正常に動作していなかったのを修正

--- spec/controllers/clips_controller_spec.rb
+++ spec/controllers/clips_controller_spec.rb
@@ -4,9 +4,36 @@ describe ClipsController do
   let (:clip) { FactoryGirl.create(:clip) }
 
   before_stub_request
-  before { clip }
+
+  describe "PUT 'create'" do
+    context "未ログインの場合" do
+      it "ログインページにリダイレクトすること" do
+        put :reclip, id: clip.id
+        response.should redirect_to(:new_user_session)
+      end
+    end
+
+    context "ログイン済みの場合" do
+      let (:image_url) { 'http://example.com/image.png' }
+      let (:image_path) { "#{Rails.root}/spec/files/rectangle.png" }
+      let (:clip_attributes) { { clip: { title: 'クリップ', url: image_url, origin_url: image_url } } }
+
+      before_sign_in
+      before { stub_request(:get, image_url).to_return(body: File.read(image_path)) }
+      before { post :create, clip_attributes }
+
+      its (:response) { should redirect_to clip_path(assigns[:clip]) }
+      its (:current_user) { should be_present }
+
+      it 'クリップの保存に成功していること' do
+        assigns[:clip].persisted?.should be_true
+      end
+    end
+  end
 
   describe "PUT 'reclip'" do
+    before { clip }
+
     context "未ログインの場合" do
       it "ログインページにリダイレクトすること" do
         put :reclip, id: clip.id
