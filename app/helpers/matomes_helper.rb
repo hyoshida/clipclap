85fe97729c622dc6c1063155aa2954d78eab388e
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-11-23 23:15:37 +0900

    リファクタリング: クリップ画像表示部をヘルパーに切り出し

--- app/helpers/matomes_helper.rb
+++ app/helpers/matomes_helper.rb
@@ -1,6 +1,5 @@
 module MatomesHelper
   def image_tag_for_matome_cover(clip)
-    style = clip.thumb_size_for_style_sheet(mode: :crop, size: 100)
-    image_tag image_path(clip.image), alt: clip.title, style: style
+    image_tag_by_clip(clip, mode: :crop, size: 100)
   end
 end
--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,11 +1,9 @@
 module TagsHelper
   def image_tag_for_tag_small(clip)
-    style = clip.thumb_size_for_style_sheet(mode: :crop, size: (Settings.thumb_width / 3.0).floor)
-    image_tag image_path(clip.image), alt: clip.title, style: style
+    image_tag_by_clip(clip, mode: :crop, size: (Settings.thumb_width / 3.0).floor)
   end
 
   def image_tag_for_tag(clip)
-    style = clip.thumb_size_for_style_sheet(mode: :crop, size: Settings.thumb_width)
-    image_tag image_path(clip.image), alt: clip.title, style: style
+    image_tag_by_clip(clip, mode: :crop, size: Settings.thumb_width)
   end
 end
