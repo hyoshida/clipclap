2c08325fe853e67312e34d996eb11cda46941744
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-11-23 23:10:11 +0900

    refs #30 まとめ: カバー画像のサイズを調整

--- app/helpers/matomes_helper.rb
+++ app/helpers/matomes_helper.rb
@@ -1,2 +1,6 @@
 module MatomesHelper
+  def image_tag_for_matome_cover(clip)
+    style = clip.thumb_size_for_style_sheet(mode: :crop, size: 100)
+    image_tag image_path(clip.image), alt: clip.title, style: style
+  end
 end
--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,11 @@
 module TagsHelper
+  def image_tag_for_tag_small(clip)
+    style = clip.thumb_size_for_style_sheet(mode: :crop, size: (Settings.thumb_width / 3.0).floor)
+    image_tag image_path(clip.image), alt: clip.title, style: style
+  end
+
+  def image_tag_for_tag(clip)
+    style = clip.thumb_size_for_style_sheet(mode: :crop, size: Settings.thumb_width)
+    image_tag image_path(clip.image), alt: clip.title, style: style
+  end
 end
