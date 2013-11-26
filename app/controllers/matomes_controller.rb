62c56a3611fea31258fd0269527c4a7f9b2052c5
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-11-26 23:34:26 +0900

    まとめ:編集: 追加済みクリップは選択できないように修正

--- app/controllers/matomes_controller.rb
+++ app/controllers/matomes_controller.rb
@@ -22,7 +22,8 @@ class MatomesController < ApplicationController
 
   # XHR GET /matomes/clips
   def clips
-    @clips = Clip.where(user_id: current_user.id).paginate(page: params[:page])
+    clip_ids = params[:matome].delete(:clip_ids)
+    @clips = Clip.where(user_id: current_user.id).where.not(id: clip_ids).paginate(page: params[:page])
     render text: "<html><body><div id='container'>#{@clips.map(&insert_div_tag_for_image_tag).join}</div></body></html>" unless first_page?
   end
 
