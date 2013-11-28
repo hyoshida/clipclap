cf39127b2b4119813c8710ac8af3fde4953f71b6
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-11-29 00:37:36 +0900

    Coveralls: 結果がUnknownになる問題を解消

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+require 'coveralls'
+Coveralls.wear!('rails')
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
@@ -9,9 +12,6 @@ require 'webmock/rspec'
 require 'capybara/rspec'
 require 'capybara-webkit'
 Capybara.javascript_driver = :webkit
-
-require 'coveralls'
-Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
