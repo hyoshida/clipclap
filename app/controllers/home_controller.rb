ceba65ca3fc2fc2f1aabb70e26804bd4805cf4e4
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-01-09 01:34:44 +0900

    トップ: 人気まとめを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
   def index
     @clips = Clip.paginate(page: params[:page]).includes([ { image: :likes, comments: :user }, :user, :likes, :tags ])
     @hot_tags = Tag.hot(50).shuffle
+    @hot_matomes = Matome.hot(5).all
     render action: 'next_page' unless first_page?
   end
 
