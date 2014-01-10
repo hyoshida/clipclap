2801b3f461b7b9c11e29ea188c7f64efb8608a5d
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-01-11 06:43:37 +0900

    ゲスト: ゲスト専用ページを廃止

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,8 @@
 class HomeController < ApplicationController
-  before_filter :guest, unless: :user_signed_in?
-
   def index
     @clips = Clip.paginate(page: params[:page]).includes([ { image: :likes, comments: :user }, :user, :likes, :tags ])
     @hot_tags = Tag.hot(50).shuffle
     @new_matomes = Matome.limit(5).all
     render action: 'next_page' unless first_page?
-  end
-
-  def guest
-    @clips = Clip.includes([ { image: :likes, comments: :user }, :user, :likes, :tags ]).reorder('view_count DESC, created_at DESC').paginate(page: params[:page])
-    if first_page?
-      render action: 'guest', layout: 'application_for_guest'
-    else
-      render action: 'next_page'
-    end
   end
 end
