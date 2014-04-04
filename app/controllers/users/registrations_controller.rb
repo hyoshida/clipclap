13c44b2398b8d546577c4f5d407f83def5604662
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-04-05 03:23:54 +0900

    新規登録: Google Analyticsにコンバージョン専用パスを送信するように変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
+    flash[:user_signup] = true
     tutorial_path
   end
 end
